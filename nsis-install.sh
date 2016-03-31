9a46dc3a1d5ca29962f7a50617e0f3f36475b3d7
Cristian MARTINEZ <martinec@scitify.com>
2016-03-31 15:28:09 +0200

    minor fix

--- nsis-install.sh
+++ nsis-install.sh
@@ -3,9 +3,10 @@
 set -eo pipefail
 
 NSIS_VERSION="3.0a2"
-NSIS_DIST="nsis-$NSIS_VERSION"
+NSIS_NAME="nsis"
+NSIS_DIST="$NSIS_NAME-$NSIS_VERSION"
 NSIS_SOURCE="$NSIS_DIST-src"
-NSIS_MIRROR="http://iweb.dl.sourceforge.net/project/nsis"
+NSIS_MIRROR="http://iweb.dl.sourceforge.net/project/$NSIS_NAME"
 NSIS_RELEASE="$NSIS_MIRROR/NSIS%203%20Pre-release/$NSIS_VERSION"
 
 patch_3_0a2() {
@@ -39,16 +40,21 @@ EOF
 
 curl -s "$NSIS_RELEASE/$NSIS_DIST.zip" -o "$NSIS_DIST.zip"
 unzip "$NSIS_DIST"
+mv "$NSIS_DIST" "$NSIS_NAME"
 export NSISDIR
-NSISDIR="$(readlink -f "$NSIS_DIST")"
+NSISDIR="$(readlink -f "$NSIS_NAME")"
 
 curl -s "$NSIS_RELEASE/$NSIS_SOURCE.tar.bz2" -o "$NSIS_SOURCE.tar.bz2"
 tar -xvf "$NSIS_SOURCE.tar.bz2"
 
-patch_3_0a2 | patch -p1 -i -
+if [ "$NSIS_VERSION" == "3.0a2" ]; then
+  patch_3_0a2 | patch -p1 -i -
+fi
 
 cd "$NSIS_SOURCE"
 scons SKIPSTUBS=all SKIPPLUGINS=all SKIPUTILS=all SKIPMISC=all NSIS_CONFIG_CONST_DATA=no PREFIX="$NSISDIR"  install-compiler
 
 export NSIS_BIN="$NSISDIR/bin/makensis"
 chmod +x "$NSIS_BIN"
+sudo mkdir -p "/usr/local/bin"
+sudo ln -s "$NSIS_BIN" "/usr/local/bin/makensis"
