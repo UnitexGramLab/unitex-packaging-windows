aea63276c6ec115d8cfa6aa068b0de8fb8abf2b6
Cristian MARTINEZ <martinec@scitify.com>
2016-03-31 03:02:00 +0200

    update nsis-install.sh

--- nsis-install.sh
+++ nsis-install.sh
@@ -8,8 +8,6 @@ install() {
   sudo dpkg -i "$filename"
 }
 
-install "nsis-common_2.50-1_all.deb"
-install "nsis_2.50-1_amd64.deb"
-install "nsis-pluginapi_2.50-1_all.deb"
-
-
+install "nsis-common_2.46-10.1_all.deb"
+install "nsis_2.46-10.1_amd64.deb"
+install "nsis-pluginapi_2.46-10.1_all.deb"
